3fc5f5306b7cab2d1ce6e49b6d4ebb28c20859dd
hutuxian <hutuxian2011@sina.cn>
2019-11-27 13:33:05 +0800

    remove GIL in pybind

--- paddle/fluid/pybind/box_helper_py.cc
+++ paddle/fluid/pybind/box_helper_py.cc
@@ -40,11 +40,11 @@ void BindBoxHelper(py::module* m) {
       .def(py::init([](paddle::framework::Dataset* dataset, int year, int month, int day) {
         return std::make_shared<paddle::framework::BoxHelper>(dataset, year, month, day);
       }))
-      .def("begin_pass", &framework::BoxHelper::BeginPass)
-      .def("end_pass", &framework::BoxHelper::EndPass)
-      .def("wait_feed_pass_done", &framework::BoxHelper::WaitFeedPassDone)
-      .def("preload_into_memory", &framework::BoxHelper::PreLoadIntoMemory)
-      .def("load_into_memory", &framework::BoxHelper::LoadIntoMemory);
+      .def("begin_pass", &framework::BoxHelper::BeginPass, py::call_guard<py::gil_scoped_release>())
+      .def("end_pass", &framework::BoxHelper::EndPass, py::call_guard<py::gil_scoped_release>())
+      .def("wait_feed_pass_done", &framework::BoxHelper::WaitFeedPassDone, py::call_guard<py::gil_scoped_release>())
+      .def("preload_into_memory", &framework::BoxHelper::PreLoadIntoMemory, py::call_guard<py::gil_scoped_release>())
+      .def("load_into_memory", &framework::BoxHelper::LoadIntoMemory, py::call_guard<py::gil_scoped_release>());
 }  // end BoxHelper
 
 void BindBoxWrapper(py::module* m) {
